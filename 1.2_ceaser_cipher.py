44b9f493a60a3e9831dea474b8ba25574068d149
daiwikm29 <daiwikm29@gmail.com>
2025-12-23 12:29:22 -0800

    completed project 1.2

--- 1.2_ceaser_cipher.py
+++ 1.2_ceaser_cipher.py
@@ -1,1 +1,35 @@
-print("hell oworld")
+import random
+
+shift_key_string = input("What is your desired shift( if you have no preference plz input 'none' ): ")
+shift_key_int = 0
+if shift_key_string == "none":
+    shift_key_int = random.randint(0, 26)
+else:
+    shift_key_int = int(shift_key_string)
+
+def encode(message):
+    encoded_message = ""
+    for i in range(len(message)):
+        if message[i] == " ":
+            encoded_message += " "
+            
+        elif message[i] != " ":
+            encoded_message += chr(ord(message[i])+shift_key_int)
+       
+    return encoded_message
+    
+def decode(message):
+    decoded_message = ""
+    for i in range(len(message)):
+        if message[i] == " ":
+            decoded_message += " "
+            
+        elif message != " ":
+            decoded_message += chr(ord(message[i])-shift_key_int)
+    return decoded_message
+
+user_mess = input("What message would you like to encode: ")
+print(encode(user_mess))
+print(decode(encode(user_mess)))
+        
+        
