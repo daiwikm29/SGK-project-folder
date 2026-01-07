e862fabc083c4f46b847243f14a1fa62be931dd1
daiwikm29 <daiwikm29@gmail.com>
2026-01-06 21:00:15 -0800

    add bonus feature of using past searches to 'add' a base case

--- 3.1_factorial_recurse.py
+++ 3.1_factorial_recurse.py
@@ -1,13 +1,16 @@
-factorialInput = input("What number would you like to take the factorial of: ")
+while True:
+    factorialInput = input("What number would you like to take the factorial of: ")
+    past_searches = {}
 
-def factorialFinder(n):
-    #base case
-    if n==0 or n==1: return 1
-    #recursive case
-    elif n>1:
-        print(n)
-        return n*factorialFinder(n-1)
+    def factorialFinder(n):
+        #base case
+        if n==0 or n==1: return 1
+        elif n in past_searches: n = past_searches[n]
+        #recursive case
+        elif n>1:
+            return n*factorialFinder(n-1)
 
-print()
+    print()
 
-print(factorialFinder(int(factorialInput)))
+    print(factorialFinder(int(factorialInput)))
+    past_searches[int(factorialInput)] = factorialFinder(int(factorialInput))
