e04d18cbcfcafb65cd3950dacf57bdf652fe916a
daiwikm29 <daiwikm29@gmail.com>
2026-01-06 20:52:50 -0800

    completed project

--- 3.1_factorial_recurse.py
+++ 3.1_factorial_recurse.py
@@ -1,1 +1,13 @@
+factorialInput = input("What number would you like to take the factorial of: ")
 
+def factorialFinder(n):
+    #base case
+    if n==0 or n==1: return 1
+    #recursive case
+    elif n>1:
+        print(n)
+        return n*factorialFinder(n-1)
+
+print()
+
+print(factorialFinder(int(factorialInput)))
